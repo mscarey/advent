b9e95a237ac12fe095c759eb128813694571edd3
Matt Carey <matt@authorityspoke.com>
2021-12-11 02:43:36 -0600

    fix bug: I depended on string[-1] raising an IndexError

--- dec11.py
+++ dec11.py
@@ -7,20 +7,23 @@ def get_data(day: int = 11, toy: bool = True) -> list[list[int]]:
 
 
 def flash(data: list[list[int]], x: int, y: int) -> list[list[int]]:
+    
     new_data = [[item for item in row] for row in data]
+    new_data[y][x] = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
             if i == 0 and j == 0:
-                new_data[y][x] = 11
                 continue
             try:
-                new_data[y + i][x + j] = min(11, new_data[y + i][x + j] + 1)
+                if new_data[y + i][x + j] != 0:
+                    if y + i >= 0 and x + j >= 0:
+                        new_data[y + i][x + j] = new_data[y + i][x + j] + 1
             except IndexError:
                 pass
     return new_data
 
 
-def step(data: list[list[int]]) -> list[list[int]]:
+def step(data: list[list[int]]) -> tuple[list[list[int]], int]:
     new_data = [[item + 1 for item in row] for row in data]
     total_flashes = 0
     new_flashes = 1
@@ -28,14 +31,10 @@ def step(data: list[list[int]]) -> list[list[int]]:
         new_flashes = 0
         for y, row in enumerate(new_data):
             for x, item in enumerate(row):
-                if item == 10:
+                if item > 9:
                     new_data = flash(new_data, x, y)
                     new_flashes += 1
         total_flashes += new_flashes
-    for y, row in enumerate(new_data):
-        for x, item in enumerate(row):
-            if item == 11:
-                new_data[y][x] = 0
     return new_data, total_flashes
 
 
--- tests/test_dec11.py
+++ tests/test_dec11.py
@@ -8,4 +8,21 @@ class TestDec11:
     def test_two_steps(self):
         data = dec11.get_data()
         grid, total_flashes = dec11.multiple_steps(data, 2)
+        assert grid[0] == [8, 8, 0, 7, 4, 7, 6, 5, 5, 5]
         assert grid[1] == [5, 0, 8, 9, 0, 8, 7, 0, 5, 4]
+        assert grid[2] == [8, 5, 9, 7, 8, 8, 9, 6, 0, 8]
+        assert grid[3] == [8, 4, 8, 5, 7, 6, 9, 6, 0, 0]
+        assert grid[4] == [8, 7, 0, 0, 9, 0, 8, 8, 0, 0]
+
+    def test_10_steps(self):
+        data = dec11.get_data()
+        grid, total_flashes = dec11.multiple_steps(data, 10)
+        assert total_flashes == 204
+
+        assert grid[1] == [0, 0, 3, 1, 1, 1, 2, 0, 0, 9]
+
+    def test_100_steps(self):
+        data = dec11.get_data()
+        grid, total_flashes = dec11.multiple_steps(data, 100)
+        assert total_flashes == 1656
+        assert grid[1] == [0, 7, 4, 9, 7, 6, 6, 9, 1, 8]
